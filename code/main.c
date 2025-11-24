4b47bb01fcb017ff9b88d6a9c105bf9c0d8172b4
Zaverpon <Maxzaveriukha@gmail.com>
2025-11-24 08:47:51 +0100

    added main to com_inti and com_fin funcs

--- code/main.c
+++ code/main.c
@@ -1,53 +1,23 @@
 #include "com.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 
-int main()
+int main(void)
 {
     int rank;
+
     com_initialize(5, &rank);
 
     if (rank == 0) {
         printf("Server: waiting for clients to finish...\n");
+        sleep(2); /* simulate some work */
         com_finalize();
         printf("Server: done.\n");
-        return 0;
+    } else {
+        printf("Client %d: doing some work...\n", rank);
+        sleep(1 + rank); /* simulate different work times */
+        com_finalize();
     }
 
-    if (rank == 1) {
-        const char* msg = "Hello from client 1!\n";
-        com_send(2, (void*)msg, strlen(msg) + 1);
-        printf("Client 1: message sent.\n");
-        sleep(1);
-    }
-    if (rank == 3){
-        const char* msg = "Hello from client 3!\n";
-        com_send(4, (void*)msg, strlen(msg) + 1);
-        printf("Client 1: message sent.\n");
-        sleep(1);
-    }
-
-    if (rank == 2) {
-        void* buff;
-        size_t size;
-
-        com_recv(&buff, &size);
-        printf("Client 2: received message:'%s'\n", (char*)buff);
-        free(buff);
-        sleep(1);
-    }
-    if (rank == 4) {
-        void* buff;
-        size_t size;
-
-        com_recv(&buff, &size);
-        printf("Client 4: received message:'%s'\n", (char*)buff);
-        free(buff);
-        sleep(1);
-    }
-
-    com_finalize();
     return 0;
 }
